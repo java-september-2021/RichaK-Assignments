e09fe1e885a2aa1c12915528abfed80a8f87caed
Richa <richa_k@hotmail.com>
2021-09-01 16:57:26 -0700

    adding more test files

--- 01-java-fundamentals/05-basics/BasicJava.java
+++ 01-java-fundamentals/05-basics/BasicJava.java
@@ -1,70 +1,81 @@
-public class BasicJava{
-    public void printNum( int number){
-        for(int i = 1 ; i <= number; i++){
-            System.out.println("The number is :" + i);  
+import java.util.ArrayList;
+
+public class BasicJava {
+    public void printNum(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.println("The number is :" + i);
         }
     }
 
-    public void oddNum(int number){
-        for(int i = 1 ; i <= number; i++){
-            if(i % 2 == 1){
-                System.out.println("The odd number is :" + i);  
-            }      
+    public void oddNum(int number) {
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
         }
 
     }
 
-    public int sum(int number){
+    public int sum(int number) {
         int sum = 0;
 
-        for(int i = 1 ; i <= number ; i++){
+        for (int i = 1; i <= number; i++) {
             sum += i;
         }
         return sum;
 
     }
 
-    public void iterateThruArray(int [] number){
-        for(int i = 0 ; i < number.length; i++){
+    public void iterateThruArray(int[] number) {
+        for (int i = 0; i < number.length; i++) {
             System.out.println("The integer on position " + i + " is :  " + number[i]);
         }
     }
 
-
-    public int findMax(int [] num){
+    public int findMax(int[] num) {
 
         int maxNum = num[0];
-        for(int i = 1; i < num.length; i++){
-            if(maxNum < num[i]){
+        for (int i = 1; i < num.length; i++) {
+            if (maxNum < num[i]) {
                 maxNum = num[i];
             }
-        }return maxNum;
+        }
+        return maxNum;
 
     }
 
-    public void oddNumbers(int num){
-        int [] newArr = new int[num];
-        for(int i = 0 ; i < newArr.length; i++){
-            if(i % 2 != 0 ){
+    public int[] oddNumbers(int num) {
+        int[] newArr = new int[num];
+        for (int i = 0; i < newArr.length; i++) {
+            if (i % 2 != 0) {
                 newArr[i] = 2 * i + 1;
-                System.out.println(newArr[i]);
+                // System.out.println(newArr[i]);
             }
         }
+        return newArr;
     }
 
-    public int getAverage(int [] numArr){
+    public void oddArray() {
+        ArrayList<Object> y = new ArrayList<>();
+        for (int i = 1; i <= 255; i += 2) {
+            y.add(i);
+        }
+        System.out.println(y);
+    }
+
+    public int getAverage(int[] numArr) {
         int sum = 0;
-        for(int i = 0; i < numArr.length; i++){
+        for (int i = 0; i < numArr.length; i++) {
             sum += numArr[i];
         }
-        int average = sum/numArr.length;
+        int average = sum / numArr.length;
         return average;
     }
 
-    public int greaterThanY(int [] numArray, int num){
+    public int greaterThanY(int[] numArray, int num) {
         int count = 0;
-        for(int i = 0 ; i < numArray.length; i++){
-            if(numArray[i] > num){
+        for (int i = 0; i < numArray.length; i++) {
+            if (numArray[i] > num) {
                 count++;
             }
         }
@@ -72,12 +83,54 @@ public class BasicJava{
 
     }
 
-    public void squareValues(int [] numArr){
-        for (int i = 0; i < numArr.length; i++){
-            //numArr[i] = numArr[i] * numArr[i];
-            //numArr[i] = (int)Math.pow(numArr[i], 2);
-            System.out.println(numArr[i] + "\t" + (int)Math.pow(numArr[i], 2) );
+    public void squareValues(int[] numArr) {
+        for (int i = 0; i < numArr.length; i++) {
+            // numArr[i] = numArr[i] * numArr[i];
+            // numArr[i] = (int)Math.pow(numArr[i], 2);
+            System.out.println(numArr[i] + "\t" + (int) Math.pow(numArr[i], 2));
         }
+    }
+
+    public void removeNegativeValue(int[] negValues) {
+        for (int i = 0; i < negValues.length; i++) {
+            if (negValues[i] < 0) {
+                negValues[i] = 0;
+            }
+            System.out.println(negValues[i]);
+        }
+    }
+
+    public int[] maxMinAverage(int[] arr) {
+        int[] newArray = new int[3];
+        // Find Max add to new array
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        newArray[0] = max;
+        // Find min assing it to new array
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        newArray[1] = min;
+
+        int sum = 0;
+
+        // Get the average and assign it to a new array
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        int average = sum / arr.length;
+        newArray[2] = average;
+
+        return newArray;
+
     }
 
 }
--- 01-java-fundamentals/05-basics/BasicTest.java
+++ 01-java-fundamentals/05-basics/BasicTest.java
@@ -1,17 +1,20 @@
-public class BasicTest{
-    public static void main(String args[]){
-        int [] numArray = {1,3,5,7,9,13};
-        int [] numArray2 = {-3,-5,-7,0};
-        int [] numArray3 = {2,10,3};
+import java.util.Arrays;
+
+public class BasicTest {
+    public static void main(String args[]) {
+        int[] numArray = { 1, 3, 5, 7, 9, 13 };
+        int[] numArray2 = { 3, -5, -7, 0, 1, -9, -4, 48 };
+        int[] numArray3 = { 2, 10, 3 };
+        int num = 255;
         BasicJava basic = new BasicJava();
         System.out.println("\n");
         System.out.println("*** Printing Number from 1 to 255 ***");
         basic.printNum(255);
         System.out.println("\n");
-        System.out.println("*** Printing  Odd Numbers ***");
+        System.out.println("*** Printing Odd Numbers ***");
         basic.oddNum(255);
         System.out.println("\n");
-        System.out.println("*** Printing  Sum  of 255 ***");
+        System.out.println("*** Printing Sum of 255 ***");
         System.out.println("The sum is : " + basic.sum(255));
         System.out.println("\n");
         System.out.println("*** Iterating thru an array ***");
@@ -23,8 +26,10 @@ public class BasicTest{
         System.out.println("*** Finding Max Num in an array of positive nums ***");
         System.out.println(basic.findMax(numArray));
         System.out.println("\n");
-        System.out.println("***Returning array with odd number ***");
-        basic.oddNumbers(255);
+        System.out.println("*** Printing array with odd numbers ***");
+        int[] odd = basic.oddNumbers(num);
+        System.out.println(Arrays.toString(odd));
+        basic.oddArray();
         System.out.println("\n");
         System.out.println("*** Printing average ***");
         System.out.println(basic.getAverage(numArray2));
@@ -42,6 +47,12 @@ public class BasicTest{
         basic.squareValues(numArray2);
         System.out.println("*** Square values ***");
         basic.squareValues(numArray);
-
+        System.out.println("\n");
+        System.out.println("*** Removing negative value with 0 ***");
+        basic.removeNegativeValue(numArray2);
+        System.out.println("\n");
+        System.out.println("*** Printing Max,Min,Average ***");
+        int[] mma = basic.maxMinAverage(numArray3);
+        System.out.println(Arrays.toString(mma));
     }
 }
